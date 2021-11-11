818ab7a20e1deaad288cf0edc429755bd56024cf
Dev <pauls@Paulss-MacBook-Pro.local>
2021-11-11 00:52:08 -0500

    add pauls

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hi my name is patric
+          hi my name is patric and pauls
         </p>
         <a
           className="App-link"
